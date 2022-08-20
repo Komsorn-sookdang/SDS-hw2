9a506587eb8ec60787d9047384832c6721d420de
มึน <komsornzaza@gmail.com>
2022-08-20 17:57:12 +0700

    first commit

---
+++ cpu_test.py
@@ -0,0 +1,14 @@
+import time
+
+time_list = []
+start_time = time.time()
+
+for i in range(50000):
+    time_list.append(time.time() - start_time)
+    
+file_name = "scenarios4 cpu_test.csv"
+f = open(file_name, "w")
+for i in range(len(time_list)):
+    f.write(str(i) + ", " + format(time_list[i], ".8f") + "\n")
+    # print(str(i) + ", " + str(time_list[i]) + "\n")
+f.close()
