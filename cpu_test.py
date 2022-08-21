b5b9e089dfb68772978a5b92e0b0e9917c989467
dseadmin <komsornzaza@gmail.com>
2022-08-21 08:01:27 +0000

    add scenario3

--- cpu_test.py
+++ cpu_test.py
@@ -6,7 +6,7 @@ start_time = time.time()
 for i in range(50000):
     time_list.append(time.time() - start_time)
     
-file_name = "scenarios2 cpu_test.csv"
+file_name = "scenarios3 cpu_test.csv"
 f = open(file_name, "w")
 for i in range(len(time_list)):
     f.write(str(i) + ", " + format(time_list[i], ".8f") + "\n")
